4f493290cefa8a6972c5a9f1fbf0d11aa527ff01
cddy <100669823@qq.com>
2022-11-15 16:53:15 +0800

    完成详情页面的编写

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,11 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/HomeView.vue')
+  },
+  {
+    path: '/info',
+    name: 'info',
+    component: () => import('../views/InfoView.vue')
   }
 ]
 
---
+++ src/views/InfoView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="info_page-box">
+    <div class="video-box">
+      <video
+        controls
+        class="video_content-box"
+        src="http://company.float.run/assets/1650859818753.mp4"
+      ></video>
+    </div>
+    <div class="thum-box"></div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped>
+.info_page-box {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: stretch;
+  flex-direction: column;
+  & > .video-box {
+    height: 0;
+    padding-bottom: 50%;
+    position: relative;
+    & > .video_content-box {
+      position: absolute;
+      left: 0;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+  & > .thum-box {
+    height: 0;
+    flex-grow: 1;
+  }
+}
+</style>
